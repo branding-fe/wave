947109217ae30254fddd99dffbbb53e430718032
songao <songao@baidu.com>
2015-01-17 12:53:34 +0800

    build script

--- build.js
+++ build.js
@@ -16,18 +16,24 @@
 
 var fs = require('fs');
 var child_process = require('child_process');
+var uglify = require('uglify-js');
 
+var package = JSON.parse(fs.readFileSync(__dirname + '/package.json', 'utf-8'));
 function merge() {
     var code = fs.readFileSync(__dirname + '/build/wrap/start.js', 'utf-8')
         + fs.readFileSync(__dirname + '/build/wrap/almond.js', 'utf-8')
         + fs.readFileSync(__dirname + '/output/asset/Wave.js', 'utf-8')
         + fs.readFileSync(__dirname + '/build/wrap/end.js', 'utf-8');
 
-    fs.writeFileSync(__dirname + '/dist/wave.js', code);
+    var minified = uglify.minify(code, {fromString: true});
+
+    fs.writeFileSync(__dirname + '/dist/wave-' + package.version + '.js', code);
+    fs.writeFileSync(__dirname + '/dist/wave.latest.js', code);
+    fs.writeFileSync(__dirname + '/dist/wave-' + package.version + '.min.js', minified.code);
+    fs.writeFileSync(__dirname + '/dist/wave.latest.min.js', minified.code);
 }
 
 child_process.exec('node node_modules/edp/bin/edp-cli build -f', function(err, stdout, stderr) {
-    console.log();
     console.log(stdout);
     console.log(stderr);
     merge();

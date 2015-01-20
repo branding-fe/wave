3ad22196d751adbcf6230420e3ae4ff46eed5c60
songao <songao@baidu.com>
2015-01-20 16:42:20 +0800

    build to amd

--- build.js
+++ build.js
@@ -20,17 +20,26 @@ var uglify = require('uglify-js');
 
 var package = JSON.parse(fs.readFileSync(__dirname + '/package.json', 'utf-8'));
 function merge() {
+    // type 1: amd module
+    var amdCode = fs.readFileSync(__dirname + '/output/asset/Wave.js', 'utf-8')
+    var minifiedCode = uglify.minify(amdCode, { fromString: true });
+
+    fs.writeFileSync(__dirname + '/dist/amd/wave-' + package.version + '.js', amdCode);
+    fs.writeFileSync(__dirname + '/dist/amd/wave.latest.js', amdCode);
+    fs.writeFileSync(__dirname + '/dist/amd/wave-' + package.version + '.min.js', minifiedCode.code);
+    fs.writeFileSync(__dirname + '/dist/amd/wave.latest.min.js', minifiedCode.code);
+
+    // type 2: output namespace in window
     var code = fs.readFileSync(__dirname + '/build/wrap/start.js', 'utf-8')
         + fs.readFileSync(__dirname + '/build/wrap/almond.js', 'utf-8')
-        + fs.readFileSync(__dirname + '/output/asset/Wave.js', 'utf-8')
+        + amdCode
         + fs.readFileSync(__dirname + '/build/wrap/end.js', 'utf-8');
-
-    var minified = uglify.minify(code, {fromString: true});
+    minifiedCode = uglify.minify(code, { fromString: true });
 
     fs.writeFileSync(__dirname + '/dist/wave-' + package.version + '.js', code);
     fs.writeFileSync(__dirname + '/dist/wave.latest.js', code);
-    fs.writeFileSync(__dirname + '/dist/wave-' + package.version + '.min.js', minified.code);
-    fs.writeFileSync(__dirname + '/dist/wave.latest.min.js', minified.code);
+    fs.writeFileSync(__dirname + '/dist/wave-' + package.version + '.min.js', minifiedCode.code);
+    fs.writeFileSync(__dirname + '/dist/wave.latest.min.js', minifiedCode.code);
 }
 
 child_process.exec('node node_modules/edp/bin/edp-cli build -f', function(err, stdout, stderr) {
